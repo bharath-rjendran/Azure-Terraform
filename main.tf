194a1f59ca2b744e0bbdce348889a6bbc677c6de
maara.hodophile@gmail.com <maara.hodophile@gmail.com>
2025-05-22 14:09:43 +0530

    Poc - 1st phase

--- main.tf
+++ main.tf
@@ -41,37 +41,37 @@ module "compute" {
   app_location = var.app_location
 }
 
-module "storage" {
-  depends_on          = [module.resource_group]
-  source              = "./modules/storage"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  environment = var.environment
-}
+# module "storage" {
+#   depends_on          = [module.resource_group]
+#   source              = "./modules/storage"
+#   resource_group_name = var.resource_group_name
+#   location            = var.location
+#   environment = var.environment
+# }
 
-module "database" {
-  depends_on = [ module.resource_group ]
-  source             = "./modules/database"
-  resource_group_name = var.resource_group_name
-  db_location = var.db_location
-  environment = var.environment
-  sql_admin_username = var.admin_username
-  sql_admin_password = var.sql_admin_password
-}
+# module "database" {
+#   depends_on = [ module.resource_group ]
+#   source             = "./modules/database"
+#   resource_group_name = var.resource_group_name
+#   db_location = var.db_location
+#   environment = var.environment
+#   sql_admin_username = var.admin_username
+#   sql_admin_password = var.sql_admin_password
+# }
 
-module "security_monitoring" {
-  depends_on = [ module.resource_group ]
-  source             = "./modules/security_monitoring"
-  resource_group_name = var.resource_group_name
-  sm_location = var.sm_location
-  environment = var.environment
-  tenant_id = var.tenant_id
-}
+# module "security_monitoring" {
+#   depends_on = [ module.resource_group ]
+#   source             = "./modules/security_monitoring"
+#   resource_group_name = var.resource_group_name
+#   sm_location = var.sm_location
+#   environment = var.environment
+#   tenant_id = var.tenant_id
+# }
 
-module "messaging" {
-  depends_on = [ module.resource_group ]
-  source             = "./modules/messaging"
-  resource_group_name = var.resource_group_name
-  msg_location = var.msg_location
-  environment = var.environment
-}
+# module "messaging" {
+#   depends_on = [ module.resource_group ]
+#   source             = "./modules/messaging"
+#   resource_group_name = var.resource_group_name
+#   msg_location = var.msg_location
+#   environment = var.environment
+# }
