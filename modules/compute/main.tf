eeb86784244931ef2f1fcf2fdc687c1777cc1889
maara.hodophile@gmail.com <maara.hodophile@gmail.com>
2025-05-22 14:56:12 +0530

    Poc -4

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-${var.environment}"
   location            = var.aks_location
   resource_group_name = var.resource_group_name
-  dns_prefix = "aks${replace(lower(trimspace(var.environment)), "-", "")}"
+  dns_prefix = "${replace("aks${lower(trimspace(var.environment))}", "-", "")}"
   kubernetes_version = "1.29.2"
 
   default_node_pool {
