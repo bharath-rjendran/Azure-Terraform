c30960a2ea84131736db48038ecb5166b5ec9438
maara.hodophile@gmail.com <maara.hodophile@gmail.com>
2025-05-22 15:07:37 +0530

    POC -6

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-${var.environment}"
   location            = var.aks_location
   resource_group_name = var.resource_group_name
-  dns_prefix = var.environment
+  dns_prefix = akspoc
   kubernetes_version = "1.29.2"
 
   default_node_pool {
@@ -63,7 +63,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 #App Services
 
 resource "azurerm_linux_web_app" "app_service" {
-  name                = "app-service-${var.environment}"
+  #name                = "app-service-${var.environment}"
+  name                = "app-service-poc"
   location            = var.app_location
   resource_group_name = var.resource_group_name
   service_plan_id = azurerm_service_plan.app_service_plan.id
