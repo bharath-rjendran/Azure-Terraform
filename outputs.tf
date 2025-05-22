f8dc2d075fc207babd2955c0e9532985b79337f7
maara.hodophile@gmail.com <maara.hodophile@gmail.com>
2025-05-22 14:16:46 +0530

    Poc - 2

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,12 @@ output "subnet_ids" {
   description = "value of subnet_ids"
   value       = module.networking.subnet_ids
 }
-output "sql_server_name" {
-  description = "The name of the SQL Server"
-  value       = module.database.sql_server_name
-}
+# output "sql_server_name" {
+#   description = "The name of the SQL Server"
+#   value       = module.database.sql_server_name
+# }
 
-output "sql_database_name" {
-  description = "The name of the SQL Database"
-  value       = module.database.sql_database_name
-}
+# output "sql_database_name" {
+#   description = "The name of the SQL Database"
+#   value       = module.database.sql_database_name
+# }
